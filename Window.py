0647dfb4ef9827b5eb879f0b4354074302edc162
daveCC_ <shelkanife884@gmail.com>
2024-03-20 18:48:15 -0600

    Añadido el Frame de saludo

--- Window.py
+++ Window.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk,Frame,Label
 
 class Window:
     def __init__(self,title) -> None:
@@ -6,3 +6,6 @@ class Window:
         self.window.title(title)
         self.window.resizable(width=False,height=False)
         self.window.geometry("400x400")
+        self.greeting_frame=Frame()
+        Label(master=self.greeting_frame,text="Hola").pack()
+        self.greeting_frame.pack()
